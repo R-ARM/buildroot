556a5c7eef92b37dbf8ddf6b90aece4250989202
Maccraft123 <maccraft123mc@gmail.com>
2022-02-04 21:31:13 +0000

    mednafen

---
+++ package/mednafen/mednafen.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# mednafen
+#
+################################################################################
+
+MEDNAFEN_VERSION = 1.29.0
+MEDNAFEN_SOURCE = mednafen-$(MEDNAFEN_VERSION).tar.xz
+MEDNAFEN_SITE = https://mednafen.github.io/releases/files
+MEDNAFEN_LICENSE = TODO
+
+MEDNAFEN_CONF_OPTS = --disable-apple2 --disable-lynx --disable-ngp --disable-pce --disable-pce-fast --disable-pcfx --disable-psx --disable-sms --disable-ss --disable-ssfplay --disable-vb --disable-wswan --enable-alsa --disable-jack
+
+$(eval $(autotools-package))
