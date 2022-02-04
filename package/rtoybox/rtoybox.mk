fa3d3c7cd17da55a07b2c1c07dc957514f044bae
Maccraft123 <maccraft123mc@gmail.com>
2022-02-04 14:41:33 +0000

    fix rtoybox

--- package/rtoybox/rtoybox.mk
+++ package/rtoybox/rtoybox.mk
@@ -4,17 +4,17 @@
 #
 ################################################################################
 
-RMENU_VERSION = main
-RMENU_SITE = git@github.com:R-ARM/Rtoybox.git
-RMENU_SITE_METHOD = git
-RMENU_DEPENDENCIES = sdl2 sdl2_ttf
-RMENU_CFLAGS = -DROS
+RTOYBOX_VERSION = main
+RTOYBOX_SITE = git@github.com:R-ARM/Rtoybox.git
+RTOYBOX_SITE_METHOD = git
+RTOYBOX_DEPENDENCIES = sdl2 sdl2_ttf
+RTOYBOX_CFLAGS = -DROS
 
-define RMENU_BUILD_CMDS
-	$(MAKE) CFLAGS=$(RMENU_CFLAGS) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+define RTOYBOX_BUILD_CMDS
+	$(MAKE) CFLAGS=$(RTOYBOX_CFLAGS) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
 endef
 
-define RMENU_INSTALL_TARGET_CMDS
+define RTOYBOX_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/Rmenu2/rmenu2 $(TARGET_DIR)/bin/rmenu
 	mkdir $(TARGET_DIR)/etc/rmenu
 	$(INSTALL) -D -m 0644 $(@D)/Rmenu2/programs $(TARGET_DIR)/etc/rmenu/programs
