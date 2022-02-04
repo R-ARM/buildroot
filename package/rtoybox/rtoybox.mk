b73590d0ee36ea28cb1ad44511689675674d36ca
Maccraft123 <maccraft123mc@gmail.com>
2022-02-04 16:02:58 +0000

    fixups

--- package/rtoybox/rtoybox.mk
+++ package/rtoybox/rtoybox.mk
@@ -4,22 +4,22 @@
 #
 ################################################################################
 
-RTOYBOX_VERSION = main
+RTOYBOX_VERSION = a175926c9cb54c60149cd28e23be68df7b8a9e01
 RTOYBOX_SITE = git@github.com:R-ARM/Rtoybox.git
 RTOYBOX_SITE_METHOD = git
 RTOYBOX_DEPENDENCIES = sdl2 sdl2_ttf
 RTOYBOX_CFLAGS = -DROS
 
 define RTOYBOX_BUILD_CMDS
-	$(MAKE) CFLAGS=$(RTOYBOX_CFLAGS) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+	$(MAKE) CFLAGS=$(RTOYBOX_CFLAGS) $(TARGET_CONFIGURE_OPTS) -C $(@D) 
 endef
 
 define RTOYBOX_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/Rmenu2/rmenu2 $(TARGET_DIR)/bin/rmenu
-	mkdir $(TARGET_DIR)/etc/rmenu
+	mkdir -p $(TARGET_DIR)/etc/rmenu
 	$(INSTALL) -D -m 0644 $(@D)/Rmenu2/programs $(TARGET_DIR)/etc/rmenu/programs
 	$(INSTALL) -D -m 0644 $(@D)/Rmenu2/emulators $(TARGET_DIR)/etc/rmenu/emulators
-	$(INSTALL) -D -m 0755 $(@D)/Rmenu2/rmedia $(TARGET_DIR)/bin/rmedia
+	$(INSTALL) -D -m 0755 $(@D)/Rmedia/rmedia $(TARGET_DIR)/bin/rmedia
 endef
 
 
