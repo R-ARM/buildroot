50ccb1e5ca1aa9ed24c0b3d6baca8c8da22dbc78
Maccraft123 <maccraft123mc@gmail.com>
2022-02-10 18:20:44 +0000

    enable psx in mednafen

--- package/mednafen/mednafen.mk
+++ package/mednafen/mednafen.mk
@@ -9,6 +9,6 @@ MEDNAFEN_SOURCE = mednafen-$(MEDNAFEN_VERSION).tar.xz
 MEDNAFEN_SITE = https://mednafen.github.io/releases/files
 MEDNAFEN_LICENSE = TODO
 
-MEDNAFEN_CONF_OPTS = --disable-apple2 --disable-lynx --disable-ngp --disable-pce --disable-pce-fast --disable-pcfx --disable-psx --disable-sms --disable-ss --disable-ssfplay --disable-vb --disable-wswan --enable-alsa --disable-jack
+MEDNAFEN_CONF_OPTS = --disable-apple2 --disable-lynx --disable-ngp --disable-pce --disable-pce-fast --disable-pcfx --enable-psx --disable-sms --disable-ss --disable-ssfplay --disable-vb --disable-wswan --enable-alsa --disable-jack
 
 $(eval $(autotools-package))
